9deaca278e9c9a46b9fd6629289be9aa7a9da979
Adoram Shoval <s1adoram@gmail.com>
2026-03-11 16:32:29 +0200

    feat: intial login and sign up design

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css' 
+import LoginPage from './pages/LoginPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LoginPage />
   </StrictMode>,
 )
