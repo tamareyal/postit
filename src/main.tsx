0fbd56b07729c6deacba7b6d245ab36208751a98
Adoram Shoval <s1adoram@gmail.com>
2026-03-12 15:01:19 +0200

    feat: add HomeFeed page constructed out of CreatePost, PostCard and LodingIndicator components

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css' 
 import AuthPage from './pages/AuthPage'
-import Header from './components/general/header'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
     <AuthPage />
   </StrictMode>,
 )
